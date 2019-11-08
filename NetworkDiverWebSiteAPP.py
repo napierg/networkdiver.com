b3295ef50d47adf806cdd8b217af3822e4dd8237
napierg <gary.napier@me.com>
2019-11-08 01:21:13 +0000

    Running

--- NetworkDiverWebSiteAPP.py
+++ NetworkDiverWebSiteAPP.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
+import sys
+
+DEBUG = False
+FREEZER_DESTINATION = "docs"
+FREEZER_DESTINATION_IGNORE = ["CNAME","404.html"]
+
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+freezer = Freezer(app)
 
 
 @app.route("/")
@@ -8,4 +18,9 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        app.debug = False
+        freezer.freeze()
+    else:
+        app.run(port=8000)
+
--- freeze.py
+++ freeze.py
@@ -5,3 +5,6 @@ freezer = Freezer(app)
 
 if __name__ == '__main__':
     freezer.freeze()
+
+
+
